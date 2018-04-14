e73a32f778a5a1e836c5dbc7b02fc516ecd83974
soareswallace <wssj2@cin.ufpe.br>
2018-04-14 00:55:39 -0300

    All bugs solved and a new database added

--- My LVQ/learning_vector_quantization.py
+++ My LVQ/learning_vector_quantization.py
@@ -1,6 +1,7 @@
 from random import randrange
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from EuclideanDistance import euclidean_distance
 
 def prototype_generator(train):
     n_instances = len(train)
@@ -9,29 +10,108 @@ def prototype_generator(train):
     return prototypes
 
 
+def inside_window(test, prot1, prot2, w):
+    di = euclidean_distance(test, prot1, test.size)
+    dj = euclidean_distance(test, prot2, test.size)
+    minimum = min(di / dj, dj / di)
+    s = ((1 - w) / (1 + w))
+
+    return (minimum > s)
+
 def train_prototypes_lvq1(train, n_prototypes, lrate, epochs):
+    print "Training data with LVQ1"
     prototypes = [prototype_generator(train) for i in range(n_prototypes)]
+    prototypes = np.array(prototypes)
+    data_copy = np.copy(train)
+    x = data_copy[:, :-1]
+    y = data_copy[:, -1]
     for epoch in range(epochs):
         rate = lrate*(1.0-(epoch/float(epochs)))
         sum_error = 0.0
         knn = NearestNeighbors(n_neighbors=1)
-        for row in train:
-            knn.fit(prototypes)
-            row = np.array(row)
-            row = row.reshape(1,-1)
-            _, nearest_prototype = knn.kneighbors(row)
-            for i in range(len(row)-1):
-                error = row[i] - nearest_prototype[i]
-                sum_error += error ** 2
-                if nearest_prototype[-1] == row[-1]:
-                    nearest_prototype += rate*error
-                else:
-                    nearest_prototype -= rate*error
+        for i in range(len(x)):
+            knn.fit(prototypes[:, :-1])
+            _, nearest_prototype = knn.kneighbors([x[i]])
+            error = x[i] - prototypes[nearest_prototype[0][0]][:-1]
+            sum_error += error ** 2
+            if prototypes[nearest_prototype[0][0]][-1] == y[i]:
+                prototypes[nearest_prototype[0][0]][:-1] += rate*error
+            else:
+                prototypes[nearest_prototype[0][0]][:-1] -= rate * error
         #print('>epoch=%d, lrate=%.3f' % (epoch, rate))
     return prototypes
 
+def train_prototypes_lvq2(prototypes, train, lrate, epochs):
+    print "Training data with LVQ2.1"
+    data_copy = np.copy(train)
+    x = data_copy[:, :-1]
+    y = data_copy[:, -1]
+    enhance = np.copy(prototypes)
+    for epoch in range(epochs):
+        rate = lrate*(1.0-(epoch/float(epochs)))
+        sum_error = 0.0
+        knn = NearestNeighbors(n_neighbors=2)
+        for i in range(len(x)):
+            knn.fit(prototypes[:, :-1])
+            _, nearest_prototype = knn.kneighbors([x[i]])
+            error = x[i] - enhance[nearest_prototype[0][0]][:-1]
+            sum_error += error ** 2
+            if inside_window(x[i], enhance[nearest_prototype[0][0]], enhance[nearest_prototype[0][0]], 0.2):
+                first = enhance[nearest_prototype[0][0]][-1]
+                second = enhance[nearest_prototype[0][1]][-1]
+                if first != second:
+                    if enhance[nearest_prototype[0][0]][-1] == y[i]:
+                        error0 = x[i] - enhance[nearest_prototype[0][0]][:-1]
+                        error1 = x[i] - enhance[nearest_prototype[0][1]][:-1]
+                        enhance[nearest_prototype[0][0]][:-1] += rate*error0
+                        enhance[nearest_prototype[0][1]][:-1] -= rate*error1
+                    if enhance[nearest_prototype[0][1]][-1] == y[i]:
+                        error0 = x[i] - enhance[nearest_prototype[0][0]][:-1]
+                        error1 = x[i] - enhance[nearest_prototype[0][1]][:-1]
+                        enhance[nearest_prototype[0][1]][:-1] += rate*error1
+                        enhance[nearest_prototype[0][0]][:-1] -= rate*error0
 
+    return enhance
+
+
+def train_prototypes_lvq3(prototypes, train, lrate, epochs):
+    print "Training data with LVQ3"
+    data_copy = np.copy(train)
+    x = data_copy[:, :-1]
+    y = data_copy[:, -1]
+    enhance = np.copy(prototypes)
+    e = 0.1
+    for epoch in range(epochs):
+        rate = lrate*(1.0-(epoch/float(epochs)))
+        sum_error = 0.0
+        knn = NearestNeighbors(n_neighbors=2)
+        for i in range(len(x)):
+            knn.fit(prototypes[:, :-1])
+            _, nearest_prototype = knn.kneighbors([x[i]])
+            error = x[i] - enhance[nearest_prototype[0][0]][:-1]
+            sum_error += error ** 2
+            if inside_window(x[i], enhance[nearest_prototype[0][0]], enhance[nearest_prototype[0][0]], 0.2):
+                first = enhance[nearest_prototype[0][0]][-1]
+                second = enhance[nearest_prototype[0][1]][-1]
+                if first != second:
+                    if enhance[nearest_prototype[0][0]][-1] == y[i]:
+                        error0 = x[i] - enhance[nearest_prototype[0][0]][:-1]
+                        error1 = x[i] - enhance[nearest_prototype[0][1]][:-1]
+                        enhance[nearest_prototype[0][0]][:-1] += rate*error0
+                        enhance[nearest_prototype[0][1]][:-1] -= rate*error1
+                    if enhance[nearest_prototype[0][1]][-1] == y[i]:
+                        error0 = x[i] - enhance[nearest_prototype[0][0]][:-1]
+                        error1 = x[i] - enhance[nearest_prototype[0][1]][:-1]
+                        enhance[nearest_prototype[0][1]][:-1] += rate*error1
+                        enhance[nearest_prototype[0][0]][:-1] -= rate*error0
+                if enhance[nearest_prototype[0][0]][-1] == y[i]:
+                    error0 = x[i] - enhance[nearest_prototype[0][0]][:-1]
+                    error1 = x[i] - enhance[nearest_prototype[0][1]][:-1]
+                    enhance[nearest_prototype[0][1]][:-1] += e*rate*error1
+                    enhance[nearest_prototype[0][0]][:-1] += e*rate*error0
+    return enhance
 
 def learning_vector_quatization(train, n_prototypes, lrate, epochs):
-    prototypes_lvq1 = train_prototypes_lvq1(train, n_prototypes, lrate, epochs)
-    return prototypes_lvq1
+    prototypes = train_prototypes_lvq1(train, n_prototypes, lrate, epochs)
+    prototypes = train_prototypes_lvq2(prototypes, train, lrate, epochs)
+    return prototypes
--- My LVQ/main.py
+++ My LVQ/main.py
@@ -1,8 +1,7 @@
 import numpy as np
-from learning_vector_quantization import train_prototypes_lvq1
+from learning_vector_quantization import train_prototypes_lvq1, train_prototypes_lvq2, train_prototypes_lvq3
 from data_handler import load_csv, data_conversion_to_float, str_column_to_int, split_data, data_balance
 from knn import getNeighbors, getResponse, getAccuracy
-from sklearn import preprocessing
 
 
 files = ['kc1.csv', 'pc1.csv']
@@ -12,7 +11,7 @@ for filename in files:
         data_conversion_to_float(df, i)
     str_column_to_int(df, len(df[0])-1)
     split = int(0.7*len(df))
-
+    kn = [1, 3]
     lvq_training_set = []
     knn_test_set = []
 
@@ -23,20 +22,47 @@ for filename in files:
 
     lvq_training_set = data_balance(lvq_training_set)
 
-
-    n_prototypes = 300
-    lrate = 0.2
-    epochs = 40
+    n_prototypes = 100
+    lrate = 0.1
+    epochs = 30
 
     prototypes_lvq1 = train_prototypes_lvq1(lvq_training_set, n_prototypes, lrate, epochs)
+    prototypes_lvq2 = train_prototypes_lvq2(prototypes_lvq1, lvq_training_set, lrate, epochs)
+    prototypes_lvq3 = train_prototypes_lvq3(prototypes_lvq2, lvq_training_set, lrate, epochs)
 
-    kn = [1, 3]
+
     for k in kn:
         predictions = []
         for row in range(len(knn_test_set)):
             neighbors = getNeighbors(prototypes_lvq1, knn_test_set[row], k)
             results = getResponse(neighbors)
             predictions.append(results)
-            #print('> predicted=' + repr(results) + ', actual=' + repr(knn_test_set[x][-1]))
         accuracy = getAccuracy(knn_test_set, predictions)
-        print('For dataset -> ' + str(filename) + ' accuracy for k= '+ str(k) + ': ' + repr(accuracy) + '%')
+        print('With LVQ1 and for dataset -> ' + str(filename) + ' accuracy for k= '+ str(k) + ': ' + repr(accuracy) + '%')
+
+    for k in kn:
+        predictions = []
+        for row in range(len(knn_test_set)):
+            neighbors = getNeighbors(prototypes_lvq2, knn_test_set[row], k)
+            results = getResponse(neighbors)
+            predictions.append(results)
+        accuracy = getAccuracy(knn_test_set, predictions)
+        print('With LVQ2.1 and for dataset -> ' + str(filename) + ' accuracy for k= '+ str(k) + ': ' + repr(accuracy) + '%')
+
+    for k in kn:
+        predictions = []
+        for row in range(len(knn_test_set)):
+            neighbors = getNeighbors(prototypes_lvq3, knn_test_set[row], k)
+            results = getResponse(neighbors)
+            predictions.append(results)
+        accuracy = getAccuracy(knn_test_set, predictions)
+        print('With LVQ3 and for dataset -> ' + str(filename) + ' accuracy for k= '+ str(k) + ': ' + repr(accuracy) + '%')
+
+    for k in kn:
+        predictions = []
+        for row in range(len(knn_test_set)):
+            neighbors = getNeighbors(lvq_training_set, knn_test_set[row], k)
+            results = getResponse(neighbors)
+            predictions.append(results)
+        accuracy = getAccuracy(knn_test_set, predictions)
+        print('Without selecting prototypes -> ' + str(filename) + ' accuracy for k= '+ str(k) + ': ' + repr(accuracy) + '%')
